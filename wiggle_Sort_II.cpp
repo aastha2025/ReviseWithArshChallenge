2301694cca8032b4a9a9eede920ece13a9d597d3
aastha <aastha2000sh@gmail.co>
2025-01-03 22:23:18 +0530

    done todays 3 ques

---
+++ wiggle_Sort_II.cpp
@@ -0,0 +1,48 @@
+/*Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+
+You may assume the input array always has a valid answer.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,1,1,6,4]
+Output: [1,6,1,5,1,4]
+Explanation: [1,4,1,5,1,6] is also accepted.
+Example 2:
+
+Input: nums = [1,3,2,2,3,1]
+Output: [2,3,1,3,1,2]
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+0 <= nums[i] <= 5000
+It is guaranteed that there will be an answer for the given input nums.*/
+
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+       // TC= O(n)  SC=  O(1)
+
+    // Find a median.
+    auto midptr = nums.begin() + n / 2;
+    nth_element(nums.begin(), midptr, nums.end());
+    int mid = *midptr;
+    
+    // Index-rewiring.
+    #define A(i) nums[(1+2*(i)) % (n|1)]
+
+    int i = 0, j = 0, k = n - 1;
+    while (j <= k) {
+        if (A(j) > mid)
+            swap(A(i++), A(j++));
+        else if (A(j) < mid)
+            swap(A(j), A(k--));
+        else
+            j++;
+    }
+    }
+};
